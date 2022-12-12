9928c71f587cd66b66b332f4f908eec96749b1da
hachimitsuboy <hachimitsu8897@icloud.com@example.com>
2022-12-13 04:34:49 +0900

    検索ボタンが押されたときに実行するメソッドを登録

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intern_trip/auth/provider/departure_date_provider.dart';
+import 'package:intern_trip/auth/provider/goal_provider.dart';
+import 'package:intern_trip/auth/provider/industry_provider.dart';
+import 'package:intern_trip/auth/provider/occupation_provider.dart';
+import 'package:intern_trip/models/database_manager.dart';
 import 'package:intern_trip/view/common_widgets/common_button.dart';
 import 'package:intern_trip/view/home_screen/children/departure_date_picker.dart';
 import 'package:intern_trip/view/home_screen/children/goal_drop_down_button.dart';
 import 'package:intern_trip/view/home_screen/children/industry_drop_down_button.dart';
 import 'package:intern_trip/view/home_screen/children/occupation_drop_down_button.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends ConsumerWidget {
+  HomeScreen({Key? key}) : super(key: key);
+  final dbManager = DatabaseManager();
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('日時検索'),
@@ -34,7 +41,16 @@ class HomeScreen extends StatelessWidget {
           const SizedBox(height: 20),
           const OccupationDropDownButton(),
           const SizedBox(height: 100),
-          CommonButton(title: '検索', onPush: () {}),
+          CommonButton(
+              title: '検索',
+              onPush: () {
+                dbManager.getInternInfo(
+                  ref.read(goalProvider),
+                  ref.read(departureDateProvider),
+                  ref.read(industryProvider),
+                  ref.read(occupationProvider),
+                );
+              }),
         ],
       ),
     );
