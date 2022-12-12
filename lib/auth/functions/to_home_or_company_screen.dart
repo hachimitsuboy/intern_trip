1fb1131cfe691688aec6fa280d2d001dfaa2e675
hachimitsuboy <hachimitsu8897@icloud.com@example.com>
2022-12-12 21:11:29 +0900

    ログイン画面において、ユーザ側・企業側それぞれの場合における画面遷移処理を行うメソッドの作成

---
+++ lib/auth/functions/to_home_or_company_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:intern_trip/utils/constants.dart';
+import 'package:intern_trip/view/company_screen/company_screen.dart';
+import 'package:intern_trip/view/home_screen/home_screen.dart';
+
+void toHomeOrCompanyScreen(UserType userType, BuildContext context) {
+  if (userType == UserType.user) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => const HomeScreen(),
+      ),
+    );
+  } else {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => const CompanyScreen(),
+      ),
+    );
+  }
+}
