af78f36089b99cb6300680d1d5c8304a45cf91af
hachimitsuboy <hachimitsu8897@icloud.com@example.com>
2022-12-13 04:26:35 +0900

    出発時を管理するプロバイダをViewとロジックで利用するため、それぞれに対して新しいプロバイダを作成した

--- lib/auth/provider/departure_date_provider.dart
+++ lib/auth/provider/departure_date_provider.dart
@@ -1,6 +1,8 @@
 import 'package:riverpod/riverpod.dart';
 
-final departureDateProvider = StateProvider<String>((ref) {
-  final nowDate = DateTime.now();
+final departureDateProvider = StateProvider<DateTime>((ref) => DateTime.now());
+
+final departureDateToStringProvider = StateProvider<String>((ref) {
+  final nowDate = ref.watch(departureDateProvider);
   return '${nowDate.year}年${nowDate.month}月${nowDate.day}日';
 });
